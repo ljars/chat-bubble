78e26fc8ad55a09df0c99fb0e1dfda4a48480518
TY3rd <72370023+TY3rd@users.noreply.github.com>
2021-06-16 22:43:01 -0700

    Add files via upload

---
+++ Chat Bubble/ChatBubble.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiplayerARPG {
+    public class ChatBubble : MonoBehaviour
+    {
+        [SerializeField] private GameObject bubbleRoot;
+        [SerializeField] private Graphic[] tintedGraphics;
+        [SerializeField] private Text messageText;
+        [SerializeField] private float minDuration = 3f;
+        [SerializeField] private float extraDurationPerCharacter = 0.1f;
+        [SerializeField] private float transitionDuration = 1f;
+
+        public Color messageTintLocal = Color.white;
+        public Color messageTintGlobal = Color.white;
+        public Color messageTintWhisper = Color.green;
+        public Color messageTintParty = Color.cyan;
+        public Color messageTintGuild = Color.blue;
+        public bool showLocalBubble = true;
+        public bool showGlobalBubble = true;
+        public bool showWhisperBubble = true;
+        public bool showPartyBubble = true;
+        public bool showGuildBubble = true;
+
+        private Vector2 defaultScale;
+        private float animationScale;
+
+        private void Awake()
+        {
+            defaultScale = bubbleRoot.transform.localScale;
+            bubbleRoot.SetActive(false);
+        }
+
+        public void Show(string message)
+        {
+            StopAllCoroutines();
+            messageText.text = message;
+            bubbleRoot.SetActive(true);
+            StartCoroutine(BubbleRoutine());
+        }
+
+        private IEnumerator BubbleRoutine()
+        {
+            // Transition in
+            animationScale = 0f;
+            while (animationScale <= 1f)
+            {
+                bubbleRoot.transform.localScale = defaultScale * animationScale;
+                animationScale += (Time.deltaTime / transitionDuration);
+                yield return null;
+            }
+
+            // Show message for given duration
+            float duration = minDuration + (extraDurationPerCharacter * messageText.text.Length);
+            yield return new WaitForSeconds(duration);
+
+            // Transition out
+            animationScale = 1f;
+            while (animationScale >= 0f)
+            {
+                bubbleRoot.transform.localScale = defaultScale * animationScale;
+                animationScale -= (Time.deltaTime / transitionDuration);
+                yield return null;
+            }
+            bubbleRoot.SetActive(false);
+        }
+
+        public bool CheckChannel(ChatChannel channel)
+        {
+            Color tint;
+
+            switch (channel) {
+                case ChatChannel.Local:
+                    if (!showLocalBubble) return false;
+                    tint = messageTintLocal;
+                    break;
+                case ChatChannel.Global:
+                    if (!showGlobalBubble) return false;
+                    tint = messageTintGlobal;
+                    break;
+                case ChatChannel.Whisper:
+                    if (!showWhisperBubble) return false;
+                    tint = messageTintWhisper;
+                    break;
+                case ChatChannel.Party:
+                    if (!showPartyBubble) return false;
+                    tint = messageTintParty;
+                    break;
+                case ChatChannel.Guild:
+                    if (!showGuildBubble) return false;
+                    tint = messageTintGuild;
+                    break;
+                default:
+                    return false;
+            }
+
+            foreach (Graphic g in tintedGraphics)
+            {
+                g.color = tint;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Chat Bubble/PlayerCharacterEntity_ChatBubble.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial class PlayerCharacterEntity
+    {
+        [Header ("Chat Bubbles")]
+        public bool showChatBubbles = true;
+
+
+        private ChatBubble bubble;
+
+        [DevExtMethods("Awake")]
+        protected void Awake_ChatBubble()
+        {
+            var bubbleObject = Instantiate(Resources.Load<GameObject>("ChatBubblePrefab"), transform);
+            bubbleObject.transform.localPosition = Vector3.zero;
+            bubble = bubbleObject.GetComponent<ChatBubble>();
+
+            // Add addon hook
+            ClientGenericActions.onClientReceiveChatMessage += ShowChatBubble;
+        }
+
+        [DevExtMethods("OnDestroy")]
+        protected void OnDestroy_ChatBubble()
+        {
+            // Remove addon hook
+            ClientGenericActions.onClientReceiveChatMessage -= ShowChatBubble;
+        }
+
+        private void ShowChatBubble(ChatMessage chatMessage)
+        {
+            if (!showChatBubbles || chatMessage.sender != CharacterName) {
+                return;
+            }
+
+            if (bubble.CheckChannel(chatMessage.channel))
+            {
+                bubble.Show(chatMessage.message);
+            }
+        }
+    }
+}
